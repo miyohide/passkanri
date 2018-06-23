0fbd063a488b9c3f500d35295bfec784ed91b11e
Hidenori Miyoshi <miyohide@gmail.com>
2018-06-23 20:53:20 +0900

    復号化処理の実装

--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 	"os"
 	"strings"
@@ -18,6 +20,15 @@ var listCmd = &cobra.Command{
 	Short: "list",
 	Long:  "list",
 	Run: func(cmd *cobra.Command, args []string) {
+		// 復号化処理の準備
+		keyText := "adfakdjfeaegfd;jdabjlkefldablkjd"
+		c, err := aes.NewCipher([]byte(keyText))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: NewCiper")
+			os.Exit(1)
+		}
+		cfbdec := cipher.NewCFBDecrypter(c, commonIV)
+
 		// 一行ごとに読み込んで、区切り文字でそれぞれ分割する
 		f, err := os.Open(".passkanri_go")
 		if err != nil {
@@ -28,7 +39,11 @@ var listCmd = &cobra.Command{
 		scanner := bufio.NewScanner(f)
 		for scanner.Scan() {
 			words := strings.Fields(scanner.Text())
-			fmt.Fprintf(os.Stdout, "%v\t%v\t%v\n", words[0], words[1], words[2])
+			cryptoPassword := words[len(words)-1]
+			// TODO これだとパスワードの長さに制限が加わるのでなんとかしたい
+			plainPassword := make([]byte, 100)
+			cfbdec.XORKeyStream(plainPassword, []byte(cryptoPassword))
+			fmt.Fprintf(os.Stdout, "%v\t%v\n", words[0], string(plainPassword))
 		}
 	},
 }
