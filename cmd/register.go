f2e0c9b4872dac811579962d2297cd2105d4b929
Hidenori Miyoshi <miyohide@gmail.com>
2018-06-27 20:12:57 +0900

    Implement new Encrypt

--- cmd/register.go
+++ cmd/register.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -36,26 +39,30 @@ var registerCmd = &cobra.Command{
 		return validateParams(*ro)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO パスワードごとにランダムにしたい
-		// 暗号化文字列
-		keyText := "adfakdjfeaegfd;jdabjlkefldablkjd"
+		// TODO cryptoのExampleから。安全ではないので、ここの部分は書き直す必要がある
+		key, _ := hex.DecodeString("6368616e676520746869732070617373")
 		passwordText := []byte(ro.RegOptPassword)
 		// 暗号化アルゴリスズムを作成
-		c, err := aes.NewCipher([]byte(keyText))
+		block, err := aes.NewCipher([]byte(key))
 		if err != nil {
-			fmt.Printf("Error: New Cipher(%d bytes) = %s", len(keyText), keyText)
+			fmt.Printf("Error: New Cipher\n")
+			os.Exit(1)
+		}
+		ciphertext := make([]byte, aes.BlockSize+len(passwordText))
+		iv := ciphertext[:aes.BlockSize]
+		if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+			fmt.Printf("Error: io.ReadFull\n")
 			os.Exit(1)
 		}
 		// 暗号化文字列の生成
-		cfb := cipher.NewCFBEncrypter(c, commonIV)
-		ciphertext := make([]byte, len(passwordText))
-		cfb.XORKeyStream(ciphertext, passwordText)
+		stream := cipher.NewCFBEncrypter(block, iv)
+		stream.XORKeyStream(ciphertext[aes.BlockSize:], passwordText)
 		file, err := os.OpenFile(".passkanri_go", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
-			fmt.Printf("File open error: %s", err)
+			fmt.Printf("File open error: %s\n", err)
 			os.Exit(1)
 		}
 		defer file.Close()
-		fmt.Fprintf(file, "%s\t%s\t%s\n", ro.RegOptName, ro.RegOptURL, ciphertext)
+		fmt.Fprintf(file, "%s\t%s\t%x\n", ro.RegOptName, ro.RegOptURL, ciphertext)
 	},
 }
