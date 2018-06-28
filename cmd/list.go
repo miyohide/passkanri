5536087fc4158fb02e43d2dc6b3e8eccfa8a30af
Hidenori Miyoshi <miyohide@gmail.com>
2018-06-28 20:54:26 +0900

    復号化処理を実装し直し

--- cmd/list.go
+++ cmd/list.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"strings"
@@ -20,30 +21,33 @@ var listCmd = &cobra.Command{
 	Short: "list",
 	Long:  "list",
 	Run: func(cmd *cobra.Command, args []string) {
-		// 復号化処理の準備
-		keyText := "adfakdjfeaegfd;jdabjlkefldablkjd"
-		c, err := aes.NewCipher([]byte(keyText))
+		// TODO cryptoのExampleから。安全ではないので、ここの部分は書き直す必要がある
+		key, _ := hex.DecodeString("6368616e676520746869732070617373")
+		block, err := aes.NewCipher([]byte(key))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: NewCiper")
 			os.Exit(1)
 		}
-		cfbdec := cipher.NewCFBDecrypter(c, commonIV)
-
 		// 一行ごとに読み込んで、区切り文字でそれぞれ分割する
 		f, err := os.Open(".passkanri_go")
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Password File can not open.")
+			fmt.Fprintf(os.Stderr, "Password File can not open.\n")
 			os.Exit(1)
 		}
 		defer f.Close()
 		scanner := bufio.NewScanner(f)
 		for scanner.Scan() {
 			words := strings.Fields(scanner.Text())
-			cryptoPassword := words[len(words)-1]
-			// TODO これだとパスワードの長さに制限が加わるのでなんとかしたい
-			plainPassword := make([]byte, 100)
-			cfbdec.XORKeyStream(plainPassword, []byte(cryptoPassword))
-			fmt.Fprintf(os.Stdout, "%v\t%v\n", words[0], string(plainPassword))
+			ciphertext, _ := hex.DecodeString(words[len(words)-1])
+			if len(ciphertext) < aes.BlockSize {
+				fmt.Fprintf(os.Stderr, "Ciphertext too short\n")
+				os.Exit(1)
+			}
+			iv := ciphertext[:aes.BlockSize]
+			ciphertext = ciphertext[aes.BlockSize:]
+			stream := cipher.NewCFBDecrypter(block, iv)
+			stream.XORKeyStream(ciphertext, ciphertext)
+			fmt.Fprintf(os.Stdout, "%v\t%v\t%v\n", words[0], words[1], string(ciphertext))
 		}
 	},
 }
