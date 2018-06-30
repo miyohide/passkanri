72c135a95a993d114e5422851a79f26e734182af
Hidenori Miyoshi <miyohide@gmail.com>
2018-06-30 23:00:33 +0900

    use SQLite3 database

--- passkanri.go
+++ passkanri.go
@@ -1,9 +1,22 @@
 package main
 
-import  (
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/miyohide/passkanri/cmd"
 )
 
-func main()  {
+func main() {
+	db, err := sql.Open("sqlite3", "db/passkanri.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS "passkanri" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255), "url" VARCHAR(255), "password" VARCHAR(255))`,
+	)
+	if err != nil {
+		panic(err)
+	}
 	cmd.Execute()
 }
