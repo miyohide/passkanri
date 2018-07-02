0880959ba2da8c4471a4d8ba921fcd85972812bd
Hidenori Miyoshi <miyohide@gmail.com>
2018-07-02 22:40:37 +0900

    use autoincrement

--- passkanri.go
+++ passkanri.go
@@ -13,7 +13,7 @@ func main() {
 		panic(err)
 	}
 	_, err = db.Exec(
-		`CREATE TABLE IF NOT EXISTS "passkanri" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255), "url" VARCHAR(255), "password" VARCHAR(255))`,
+		`CREATE TABLE IF NOT EXISTS "passkanri" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "name" VARCHAR(255), "url" VARCHAR(255), "password" VARCHAR(255))`,
 	)
 	if err != nil {
 		panic(err)
